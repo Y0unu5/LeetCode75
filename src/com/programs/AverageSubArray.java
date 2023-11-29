43df5f48d5b01e4a16ebb2a3cd4764d73ecee409
Mahmad Yunus <mkyounus541@gmail.com>
2023-11-29 10:55:31 +0530

    Solution for Maximum Average SubArray

--- src/com/programs/AverageSubArray.java
+++ src/com/programs/AverageSubArray.java
@@ -3,17 +3,27 @@ package com.programs;
 public class AverageSubArray
 {
     public static double findMaxAverage(int[] nums, int k){
-        int currentSum=0;
-        for (int i=0; i<k; ++i){
+        /**
+         * Initialized currentSum=0
+         * 1st for loop: Initially currentSum is Cal for first k elements
+         * Re assigning currentSum to maxSum
+         *2nd for loop: iterates the remaining elements of the array.
+         * At each iteration the currentSum is updated by subtracting the value of the
+         * element leaving the sliding window (element at index i-k) and adding the value of
+         * the element entering the sliding window (element at index i)
+         * The maxSum is updated whenever the current currentSum is greater than the current maxSum.
+         */
+        double currentSum=0;
+        for (int i=0; i<k; i++){
             currentSum += nums[i];
         }
-        int maxSum = currentSum;
-        for (int i=k; i< nums.length; ++i){
+        double maxSum = currentSum;
+        for (int i=k; i< nums.length; i++){
             currentSum +=(nums[i] - nums[i-k]);
             maxSum= Math.max(maxSum, currentSum);
         }
 
-        return (double) maxSum /k;
+        return  maxSum /k;
     }
 
     public static void main(String[] args) {
